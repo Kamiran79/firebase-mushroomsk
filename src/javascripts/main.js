c0fecbe729101183957d07f1c406c9d1db90eff0
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-06-20 12:36:52 -0500

    auth ki-sub

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,15 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
+import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/myNavbar/myNavbar';
 
-console.error('it is connected firebase project');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  auth.loginButton();
+  authData.checkLoginStatus();
+  myNavbar.logoutEvent();
+};
+
+init();
